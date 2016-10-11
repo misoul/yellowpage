9cc3bb5439cdef0bd59749d3065bb0c3d0835cc8
Vinh To <misoul@gmail.com>
2016-10-11 10:55:13 -0700

    Rename companies.js to Companies to be consistent

--- public/index.js
+++ public/index.js
@@ -4,12 +4,12 @@ import { Router, Route, hashHistory } from 'react-router'
 import App from './scripts/App'
 import About from './scripts/About'
 import Repos from './scripts/Repos'
-import companies from './scripts/companies'
+import Companies from './scripts/Companies'
 
 render((
   <Router history={hashHistory}>
     <Route path="/" component={App}/>
     <Route path="/repos" component={Repos}/>
-    <Route path="/about" component={companies}/>
+    <Route path="/about" component={Companies}/>
   </Router>
 ), document.getElementById('content'))
