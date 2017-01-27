d4da1bf3a5ccf93954bbc89a8b00f272d9a2a0d7
Vinh To <misoul@gmail.com>
2017-01-26 17:27:17 -0800

    Add initial version for mysql db init

---
+++ database/init_db.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE IF NOT EXISTS testdb1;
+
+USE testdb1;
+
+CREATE TABLE `companies` (
+	`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50),
+    `industries` VARCHAR(50),
+    `website` VARCHAR(50),
+    `found_date` DATETIME,
+    `stock_code` VARCHAR(50),
+    `desc` VARCHAR(100)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='companies';
+
+CREATE TABLE `comments` (
+	`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `author` VARCHAR(50),
+    `test` VARCHAR(200)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='comments';
