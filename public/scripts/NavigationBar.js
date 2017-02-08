3298c93bfe71f5575f10b1a317f666968b23b752
Vinh To <misoul@gmail.com>
2017-02-08 15:29:21 -0800

    Use bootstrap's navigationBar for a prettier look

--- public/scripts/NavigationBar.js
+++ public/scripts/NavigationBar.js
@@ -4,15 +4,23 @@ import { Link } from 'react-router'
 export default React.createClass({
   render() {
     return (
-      <ul role="nav" className="navigationBar">
-        <Link to="/about" activeClassName="active">About</Link>
-        --
-        <Link to="/companies" activeClassName="active">Companies</Link>
-        --
-        <Link to="/company/12" activeClassName="active">Company</Link>
-        --
-        <Link to="/comments" activeClassName="active">Comments</Link>
-      </ul>
+      <nav role="nav" className="navigationBar navbar navbar-default navbar-static-top">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <a className="navbar-brand" href="#">YellowPage</a>
+          </div>
+          <ul className="nav navbar-nav">
+            <li><Link to="/about" activeClassName="active">About</Link></li>
+            <li><Link to="/companies" activeClassName="active">Companies</Link></li>
+            <li><Link to="/company/12" activeClassName="active">Company</Link></li>
+            <li><Link to="/comments" activeClassName="active">Comments</Link></li>
+          </ul>
+          <ul className="nav navbar-nav navbar-right">
+            <li><a href="#/signup"><span className="glyphicon glyphicon-user"></span> Sign Up</a></li>
+            <li><a href="#/login"><span className="glyphicon glyphicon-log-in"></span> Login</a></li>
+          </ul>
+        </div>
+      </nav>
     )
   }
 })
